2b783cfb76494008398b4323e4741e096f7e102d
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-03-23 21:59:40 -0500

    feat: add validations the field forms

--- app/src/main/java/com/example/instagram_app/login/view/LoginActivity.kt
+++ app/src/main/java/com/example/instagram_app/login/view/LoginActivity.kt
@@ -1,11 +1,15 @@
 package com.example.instagram_app.login.view
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.instagram_app.R
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
@@ -19,7 +23,32 @@ class LoginActivity : AppCompatActivity() {
             insets
         }
 
-        findViewById<TextInputLayout>(R.id.login_edit_email_input)
-            .error = "Esse e-mail é inválido"
+        val editTextEmail = findViewById<TextInputEditText>(R.id.login_edit_email)
+        val editTextPassword = findViewById<TextInputEditText>(R.id.login_edit_senha)
+
+        editTextEmail.addTextChangedListener(walcher)
+        editTextPassword.addTextChangedListener(walcher)
+
+        findViewById<Button>(R.id.login_btn_enter).setOnClickListener {
+            findViewById<TextInputLayout>(R.id.login_edit_email_input).error = "E-mail é inválido"
+            findViewById<TextInputLayout>(R.id.login_edit_senha_input).error = "Senha é inválida"
+        }
+    }
+
+    private val walcher = object : TextWatcher {
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+            findViewById<Button>(R.id.login_btn_enter)
+                .isEnabled = s.toString().isNotEmpty()
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
     }
 }
