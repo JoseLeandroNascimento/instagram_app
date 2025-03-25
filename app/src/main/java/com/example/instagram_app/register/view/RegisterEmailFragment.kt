457ad0cd0c04d0a5737b86a4e90ca9e0ee0a85cc
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-03-24 21:58:33 -0500

    feat: create layout the register email

---
+++ app/src/main/java/com/example/instagram_app/register/view/RegisterEmailFragment.kt
@@ -0,0 +1,20 @@
+package com.example.instagram_app.register.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.instagram_app.R
+
+class RegisterEmailFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_register_email,container,false)
+    }
+}
