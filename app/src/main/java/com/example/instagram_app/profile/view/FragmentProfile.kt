ff2a69b3ff8b094b14c5a3eb3c2508f32a664e3a
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-03-27 17:08:20 -0500

    feat: create screen the profile

---
+++ app/src/main/java/com/example/instagram_app/profile/view/FragmentProfile.kt
@@ -0,0 +1,19 @@
+package com.example.instagram_app.profile.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.instagram_app.R
+
+class FragmentProfile : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_profile, container, false)
+    }
+}
