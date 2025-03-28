a2131d99170a0c1002b1915007f63c321c523488
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-03-28 09:51:21 -0500

    feat: add color status bar

--- app/src/main/java/com/example/instagram_app/main/view/MainActivity.kt
+++ app/src/main/java/com/example/instagram_app/main/view/MainActivity.kt
@@ -1,13 +1,18 @@
 package com.example.instagram_app.main.view
 
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsetsController
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.instagram_app.R
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.R)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,5 +22,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS,WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+        window.statusBarColor = ContextCompat.getColor(this,R.color.gray)
     }
 }
