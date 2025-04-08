d49648400e2d7c80544f6e16451d3f5757235402
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-04-07 21:22:37 -0500

    feat: tirar foto

---
+++ app/src/main/java/com/example/instagram_app/camera/view/FragmentCamera.kt
@@ -0,0 +1,21 @@
+package com.example.instagram_app.camera.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.instagram_app.R
+
+class FragmentCamera : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_camera, container, false)
+    }
+
+
+}
